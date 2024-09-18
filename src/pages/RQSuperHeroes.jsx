14db04f3432910de1c2bc62ae38447c1ddf3d5b5
pushkar1995 <pushkar.gautam1994@gmail.com>
2024-09-18 21:04:06 +0545

    RQ Tut 4: Handling Query Error

--- src/pages/RQSuperHeroes.jsx
+++ src/pages/RQSuperHeroes.jsx
@@ -7,10 +7,14 @@ const fetchSuperHeroes = () => {
 }
 
 const RQSuperHeroes = () => {
-    const { isLoading, data } = useQuery('super-heroes', fetchSuperHeroes)
+    const { isLoading, data, isError, error } = useQuery('super-heroes', fetchSuperHeroes)
 
     if (isLoading) {
         return <h2>Loading....</h2>
+    }
+
+    if (error) {
+        return <h2>{error.message}</h2>
     }
 
   return (
