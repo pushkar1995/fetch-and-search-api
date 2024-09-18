7644fcd7ccf432036b06d4af68b5c4486afbcf71
pushkar1995 <pushkar.gautam1994@gmail.com>
2024-09-18 20:51:33 +0545

    RQ Tut 3: Fetching Data with useQuery

--- src/pages/RQSuperHeroes.jsx
+++ src/pages/RQSuperHeroes.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import axios from 'axios'
 import { useQuery } from 'react-query'
 
+const fetchSuperHeroes = () => { 
+    return axios.get('http://localhost:4000/superheroes')
+}
+
 const RQSuperHeroes = () => {
-    const { isLoading, data } = useQuery('super-heroes', () => { 
-        return axios.get('http://localhost:4000/superheroes')
-    })
+    const { isLoading, data } = useQuery('super-heroes', fetchSuperHeroes)
 
     if (isLoading) {
         return <h2>Loading....</h2>
