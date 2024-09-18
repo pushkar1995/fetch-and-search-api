de45cc0b2a48370fa7e3da1b38ce064771a7757d
pushkar1995 <pushkar.gautam1994@gmail.com>
2024-09-18 22:27:09 +0545

    RQ Tut 5: Implementing React Query Devtools

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 //Create a client
 const queryClient = new QueryClient()
@@ -13,5 +14,6 @@ createRoot(document.getElementById('root')).render(
     <StrictMode>
         <App />
     </StrictMode>
+      <ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
   </QueryClientProvider>    
 )
