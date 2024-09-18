0fc830f536966bd615cf500749e3b075408d4d42
pushkar1995 <pushkar.gautam1994@gmail.com>
2024-09-18 20:30:26 +0545

    React Query Tutorial 2 : Project Setup

--- src/pages/SuperHeros.jsx
+++ src/pages/SuperHeros.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 
 const SuperHeros = () => {
+    const [isLoading, setIsLoading] = useState(true)
+    const [data, setData] = useState()
+
+    useEffect(() => { 
+        axios.get('http://localhost:4000/superheroes').then((res) => { 
+            setData(res.data)
+            setIsLoading(false)
+        })
+    }, [])
+
+    if (isLoading) {
+        return <h2>Loading....</h2>
+    }
+
   return (
-    <div>SuperHeros</div>
+    <div className='flex flex-col m-16'>
+        <h2>Super Heros Page!!!</h2>
+        {data.map((hero) => {
+            return <div key={hero.name}>{hero.name}</div>
+         }) }
+    </div>
   )
 }
 
