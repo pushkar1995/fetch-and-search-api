79d27674bed44a6762f03fa9902564f00b1d829b
pushkar1995 <pushkar.gautam1994@gmail.com>
2024-09-24 13:50:22 +0545

    Added useQuery on click functionality

--- src/pages/RQSuperHeroes.jsx
+++ src/pages/RQSuperHeroes.jsx
@@ -7,9 +7,17 @@ const fetchSuperHeroes = () => {
 }
 
 const RQSuperHeroes = () => {
-    const { isLoading, data, isError, error } = useQuery('super-heroes', fetchSuperHeroes)
+    const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
+        'super-heroes', 
+        fetchSuperHeroes,
+        {
+            enabled: false,
+        }
+    )
 
-    if (isLoading) {
+    console.log({ isLoading, isFetching})
+
+    if (isLoading || isFetching) {
         return <h2>Loading....</h2>
     }
 
@@ -20,6 +28,7 @@ const RQSuperHeroes = () => {
   return (
     <div className='flex flex-col m-16'>
         <h2 className='font-bold'>RQSuperHeroes Page!!!</h2>
+        <button className='border-2 border-black' onClick={refetch}>Fetch Heroes</button>
         {data?.data.map((hero) => {
             return <div key={hero.name}>{hero.name}</div>
          })}
