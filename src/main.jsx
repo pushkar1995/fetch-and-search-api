cd22851d87845f6605050547fe78b7ede198383a
pushkar1995 <pushkar.gautam1994@gmail.com>
2024-10-02 09:55:42 +0545

    Removing unnecessary files

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 import { QueryClient, QueryClientProvider } from 'react-query';
