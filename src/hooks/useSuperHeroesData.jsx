9d0ce951ee19b944ad02ca178839180e311d3113
pushkar1995 <pushkar.gautam1994@gmail.com>
2024-10-02 15:00:37 +0545

    Implemented Handling Mutation Response

--- src/hooks/useSuperHeroesData.jsx
+++ src/hooks/useSuperHeroesData.jsx
@@ -27,8 +27,14 @@ export const useSuperHeroesData = (onSuccess, onError) => {
 export const useAddSuperHeroData = ( ) => { 
     const queryClient = useQueryClient()
     return useMutation(addSuperHero, { 
-        onSuccess: () => { 
-            queryClient.invalidateQueries('super-heroes')
+        onSuccess: (data) => { 
+            // queryClient.invalidateQueries('super-heroes')
+            queryClient.setQueryData('super-heroes', (oldQueryData) => { 
+                return { 
+                    ...oldQueryData,
+                    data: [...oldQueryData.data, data.data]
+                }
+            })
         }
     })
 }
