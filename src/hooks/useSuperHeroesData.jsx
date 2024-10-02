2818266839966ad8c46eedc782d54f8741f0fa59
pushkar1995 <pushkar.gautam1994@gmail.com>
2024-10-02 15:23:43 +0545

    Implemented Optimistic Updates Video 24

--- src/hooks/useSuperHeroesData.jsx
+++ src/hooks/useSuperHeroesData.jsx
@@ -27,14 +27,36 @@ export const useSuperHeroesData = (onSuccess, onError) => {
 export const useAddSuperHeroData = ( ) => { 
     const queryClient = useQueryClient()
     return useMutation(addSuperHero, { 
-        onSuccess: (data) => { 
-            // queryClient.invalidateQueries('super-heroes')
+        // onSuccess: (data) => { 
+        //     // queryClient.invalidateQueries('super-heroes')
+        //     queryClient.setQueryData('super-heroes', (oldQueryData) => { 
+        //         return { 
+        //             ...oldQueryData,
+        //             data: [...oldQueryData.data, data.data]
+        //         }
+        //     })
+        // }
+        onMutate: async (newHero) => { 
+            await queryClient.cancelQueries('super-heroes')
+            const previousHeroData = queryClient.getQueryData('super-heroes')
             queryClient.setQueryData('super-heroes', (oldQueryData) => { 
-                return { 
-                    ...oldQueryData,
-                    data: [...oldQueryData.data, data.data]
-                }
-            })
-        }
+                        return { 
+                            ...oldQueryData,
+                            data: [
+                                ...oldQueryData.data, 
+                                { id: oldQueryData?.data?.length + 1, ...newHero },
+                            ],
+                        }
+                    })
+                    return { 
+                        previousHeroData
+                    }
+                },
+                onError: (_error, _hero, context) => { 
+                    queryClient.setQueryData('super-heroes', context.previousHeroData)
+                },
+                onSettled: () => {
+                    queryClient.invalidateQueries('super-heroes')
+                },
     })
 }
