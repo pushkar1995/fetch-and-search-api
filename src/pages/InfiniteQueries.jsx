e772cd5aa669eafbc33b7256d319869dc4c1a9e0
pushkar1995 <pushkar.gautam1994@gmail.com>
2024-10-02 12:45:22 +0545

    Implementing Infinite Queries

--- src/pages/InfiniteQueries.jsx
+++ src/pages/InfiniteQueries.jsx
@@ -1,12 +1,25 @@
-import { useQuery } from 'react-query'
+import { useInfiniteQuery } from 'react-query'
 import axios from 'axios'
+import { Fragment } from 'react'
 
-const fetchColors = () => { 
-    return axios.get(`http://localhost:4000/colors`)
+const fetchColors = ({ pageParam = 1  }) => { 
+    return axios.get(`http://localhost:4000/colors?_limit=2&_page=${pageParam}`)
 }
 
 const InfiniteQueries = () => {
-    const { isLoading, data, error, isError } = useQuery(['colors'],fetchColors)
+    const { isLoading, data, error, isError, hasNextPage, fetchNextPage, isFetching, isFetchingNextPage } = useInfiniteQuery(
+        ['colors'],
+        fetchColors,
+        { 
+            getNextPageParam: (_lastPage, pages) => { 
+                if (pages.length < 4) { 
+                    return pages.length + 1
+                } else { 
+                    return undefined
+                }
+            }
+        }
+    )
 
     if (isLoading) {
         return <h2>Loading....</h2>
@@ -20,16 +33,28 @@ const InfiniteQueries = () => {
     <div className='flex flex-col m-16'>
         <h1 className='font-bold'>Infinite Queries Page!!!</h1>
         <div>
-            {data?.data.map((color) => { 
+            {data?.pages.map((group, i) => { 
             return (
-                <div key={color.id}>
-                <h2>
-                    {color.id}. {color.label}
-                </h2>
-                </div>
+                <Fragment key={i}>
+                    {group.data.map((color) => (
+                        <h2 key={color.id}>
+                            {color.id} {color.label} 
+                        </h2>
+                    ))}
+                </Fragment>
             )
             })}
         </div>
+        <div>
+            <button
+                className='border ml-3 mt-6 p-3 bg-red-600'  
+                disabled={!hasNextPage} 
+                onClick={fetchNextPage}
+            >
+            Load more
+            </button>
+        </div>
+        <div>{isFetching && !isFetchingNextPage ? 'Fetching...' : null}</div>
     </div>
   )
 }
